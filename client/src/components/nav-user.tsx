f184196571c84b7fccb74f762dd9ddfa6f6f3c24
anandajuliar <anandajulia001@gmail.com>
2026-02-14 12:09:02 +0700

    feat: implement router navigation for user profile menu items

--- client/src/components/nav-user.tsx
+++ client/src/components/nav-user.tsx
@@ -120,22 +120,22 @@ export function NavUser({
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push("/dashboard/upgrade")}>
                 <Sparkles />
                 Upgrade to Pro
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push("/dashboard/account")}>
                 <BadgeCheck />
                 Account
               </DropdownMenuItem>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push("/dashboard/billing")}>
                 <CreditCard />
                 Billing
               </DropdownMenuItem>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push("/dashboard/notif")}>
                 <Bell />
                 Notifications
               </DropdownMenuItem>
